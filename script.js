7906151399863db76fb5ce5ce7cad6b6c3f3ad7f
Anik <sikder35-1408@diu.edu.bd>
2022-04-12 12:41:40 +0600

    Project setup

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons =  document.getElementById('buttons');
+
+sounds.forEach(function (sound) {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', function () {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    })
+
+    buttons.appendChild(btn);
+})
+
+function stopSongs() {
+    sounds.forEach(function (sound) {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
